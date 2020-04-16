3ba1e357d1ce5b90a4e89837cf95530c7bd4a96b
tippitytapp <marc.tapp@gmail.com>
2020-04-16 08:53:04 -0400

    got some styling done

--- app/src/components/Characters.js
+++ app/src/components/Characters.js
@@ -6,10 +6,17 @@ const Characters = props => {
         <div>
         {props.superheros.map(item => {
             console.log("foreach:", item)
-           return( <>
-                <h3>Character: {item.name}</h3>
-                <img src={item.image}/>
-            </>)
+           return(  <div className='character'>
+                        <img src={item.image} height={200}/>
+                        <div className="character_info">
+                            <h3>{item.name}</h3>
+                            <h5>Species: {item.species}</h5>
+                            <h5>Gender: {item.gender}</h5>
+                            <h5>Location: {item.location.name}</h5>
+                            <h5>Status: {item.status}</h5>
+                            <h5>Type: {item.type}</h5>
+                        </div>
+                    </div>)
         })}
         </div>
     )
