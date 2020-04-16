9a25033f069362ef225d24b9aebc8932b6b5473b
tippitytapp <marc.tapp@gmail.com>
2020-04-16 14:05:52 -0400

    working on it

--- app/src/store/actions/superherosActions.js
+++ app/src/store/actions/superherosActions.js
@@ -1,24 +1,12 @@
 import axios from 'axios';
-import React, {useState} from 'react';
 // https://superheroapi.com/api/
 // access-token: 10163371696990125
-export function ChangePage(){
-const [page, setPage] = useState(1)
-const pageUpHandler = (e) => {
-    e.preventDefault();
-    setPage(page + 1 )
-}
-const pageDownHandler = (e) => {
-    e.preventDefault();
-    setPage(page - 1)
-}
-return (page, pageUpHandler, pageDownHandler)
-}
+
 export const fetchSuperheros = () => {
     return dispatch => {
         dispatch ({type: 'FETCH_SUPER_START'});
         axios
-            .get(`https://rickandmortyapi.com/api/character/?page=${ChangePage.page}`)
+            .get('https://rickandmortyapi.com/api/character/')
             .then(res => {
                 console.log(res.data.info);
                 dispatch({type:'FETCH_SUPER_SUCCESS', payload: res.data.results})
@@ -36,7 +24,7 @@ export const fetchNextPage = () => {
     return dispatch => {
         dispatch ({type: 'FETCH_NEXT_SUPER_START'});
         axios
-            .get(`https://rickandmortyapi.com/api/character/?page=${ChangePage.pageUpHandler()}`)
+            .get(`https://rickandmortyapi.com/api/character/?page=2`)
             .then(res => {
                 dispatch({type:'FETCH_NEXT_SUPER', payload: res.data.results})
             })
